d062d4f082bf72a24e50b0a813abed4a54d9dd7a
Andreas Rodahl <andreas.rodahl@revolve.no>
2024-11-11 13:27:55 +0100

    initial kf implementation

---
+++ part_2_to_5/part5/kf_iteration.m
@@ -0,0 +1,18 @@
+function [x_pst,P_pst,x_prd,P_prd] = kf_iteration(x_prd,P_prd,Ad,Bd,Cd,psi_meas,rudder_meas)
+
+persistent Q R
+if(isempty(Q))
+    Q = [1 0 0;...
+         0 1 0;...
+         0 0 1];
+
+    R = 1;
+end
+
+K = P_prd*Cd' / (Cd*P_prd*Cd' + R);
+x_pst = x_prd + K*(psi_meas-Cd*x_prd);
+P_pst = (eye(3)-K*Cd)*P_prd*(eye(3)-K*Cd)' + K*R*K';
+x_prd = Ad*x_pst + Bd*rudder_meas;
+P_prd = Ad*P_pst*Ad' + Q;
+end
+
